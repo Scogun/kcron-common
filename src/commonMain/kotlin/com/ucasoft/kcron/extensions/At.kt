10c67919270be589eb147cc044024c1dd854450d
Sergey Antonov <AntonovSergeyS@gmail.com>
2020-11-02 01:22:53 +0100

    Restying

--- src/commonMain/kotlin/com/ucasoft/kcron/extensions/At.kt
+++ src/commonMain/kotlin/com/ucasoft/kcron/extensions/At.kt
@@ -2,9 +2,7 @@ package com.ucasoft.kcron.extensions
 
 class At(private val starting: Int, private val every: Int) {
 
-    override fun toString(): String {
-        return "$starting/$every"
-    }
+    override fun toString() = "$starting/$every"
 }
 
 infix fun Int.at(every: Int) : At = At(this, every)
