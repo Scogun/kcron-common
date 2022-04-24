008a7e9e65a688b1d0ec323c393f643cd4788ed3
Sergey Antonov <AntonovSergeyS@gmail.com>
2022-04-24 15:03:51 +0200

    Refactor builders

--- src/commonMain/kotlin/com/ucasoft/kcron/builders/MinutesBuilder.kt
+++ src/commonMain/kotlin/com/ucasoft/kcron/builders/MinutesBuilder.kt
@@ -2,8 +2,6 @@ package com.ucasoft.kcron.builders
 
 class MinutesBuilder : TimeBuilder() {
 
-    override var defaultEnd = 59
-
     val minutes : List<Int>
         get() {
             return values
