50202f5811b80493f5bbedfa382ff5543c3bca97
Sergey Antonov <AntonovSergeyS@gmail.com>
2022-04-30 22:23:22 +0200

    Extend and add tests

--- src/commonTest/kotlin/com/ucasoft/kcron/builders/BuilderTests.kt
+++ src/commonTest/kotlin/com/ucasoft/kcron/builders/BuilderTests.kt
@@ -1,24 +1,18 @@
 package com.ucasoft.kcron.builders
 
 import com.ucasoft.kcron.common.DayOfWeekGroups
+import com.ucasoft.kcron.common.WeekDays
 import com.ucasoft.kcron.extensions.*
 import kotlinx.datetime.*
-import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNull
 
 class BuilderTests {
 
-    private lateinit var builder : Builder
-
-    @BeforeTest
-    fun setupOnce() {
-        builder = Builder()
-    }
-
     @Test
     fun buildMany() {
+        val builder = Builder()
         assertEquals("* * * ? * * *", builder.expression)
         builder.days(1)
         assertEquals("* * * 1 * ? *", builder.expression)
@@ -35,12 +29,14 @@ class BuilderTests {
         builder.months(2).lastWorkDay()
         assertEquals(expected.plusDays(57), builder.nextRun)
         builder.daysOfWeek(DayOfWeekGroups.EveryStartingAt, "6/1")
-        assertEquals(expected.plusDays(36), builder.nextRun)
+        result = builder.nextRunList(1)
+        assertEquals(DayOfWeek.SUNDAY, result[0].dayOfWeek)
+        assertEquals(expected.plusDays(31), result[0])
         builder.hours(0).minutes(0).seconds(0).daysOfWeek(2, 4, 6)
         result = builder.nextRunList(3)
-        assertEquals(expected.plusDays(32), result[0])
-        assertEquals(expected.plusDays(34), result[1])
-        assertEquals(expected.plusDays(36), result[2])
+        assertEquals(expected.plusDays(33), result[0])
+        assertEquals(expected.plusDays(35), result[1])
+        assertEquals(expected.plusDays(37), result[2])
         builder.nearestWorkDay(30)
         assertNull(builder.nextRun)
         builder.months(5).nearestWorkDay(31)
@@ -62,6 +58,35 @@ class BuilderTests {
         builder.seconds(0).months(1).daysOfWeek(1 at 6)
         assertEquals("0 0 0 ? 1 6/1 2099", builder.expression)
         result = builder.nextRunList(2)
+        assertEquals(expected.plusDays(2), result[0])
+        assertEquals(expected.plusDays(3), result[1])
+        builder.daysOfWeek(3 at 6)
+        assertEquals("0 0 0 ? 1 6/3 2099", builder.expression)
+        result = builder.nextRunList(2)
+        assertEquals(expected.plusDays(2), result[0])
+        assertEquals(expected.plusDays(9), result[1])
+        builder.daysOfWeek(6 on 5)
+        assertEquals("0 0 0 ? 1 6#5 2099", builder.expression)
+        assertEquals(expected.plusDays(30), builder.nextRun)
+    }
+
+    @Test
+    fun buildManyFirstWeekDayIsSunday() {
+        val builder = Builder(WeekDays.Sunday)
+        assertEquals("* * * ? * * *", builder.expression)
+        val expected = LocalDateTime(2099, 1, 1, 0, 0)
+        builder.years(2099).months(2).daysOfWeek(DayOfWeekGroups.EveryStartingAt, "6/1")
+        var result = builder.nextRunList(1)
+        assertEquals(DayOfWeek.FRIDAY, result[0].dayOfWeek)
+        assertEquals(expected.plusDays(36), result[0])
+        builder.hours(0).minutes(0).seconds(0).daysOfWeek(2, 4, 6)
+        result = builder.nextRunList(3)
+        assertEquals(expected.plusDays(32), result[0])
+        assertEquals(expected.plusDays(34), result[1])
+        assertEquals(expected.plusDays(36), result[2])
+        builder.seconds(0).months(1).daysOfWeek(1 at 6)
+        assertEquals("0 0 0 ? 1 6/1 2099", builder.expression)
+        result = builder.nextRunList(2)
         assertEquals(expected.plusDays(1), result[0])
         assertEquals(expected.plusDays(2), result[1])
         builder.daysOfWeek(3 at 6)
@@ -72,5 +97,34 @@ class BuilderTests {
         builder.daysOfWeek(7 on 5)
         assertEquals("0 0 0 ? 1 7#5 2099", builder.expression)
         assertEquals(expected.plusDays(30), builder.nextRun)
+    }
+
+    @Test
+    fun buildManyFirstWeekDayIsWednesday() {
+        val builder = Builder(WeekDays.Wednesday)
+        assertEquals("* * * ? * * *", builder.expression)
+        val expected = LocalDateTime(2099, 1, 1, 0, 0)
+        builder.years(2099).months(2).daysOfWeek(DayOfWeekGroups.EveryStartingAt, "6/1")
+        var result = builder.nextRunList(1)
+        assertEquals(DayOfWeek.MONDAY, result[0].dayOfWeek)
+        assertEquals(expected.plusDays(32), result[0])
+        builder.hours(0).minutes(0).seconds(0).daysOfWeek(2, 4, 6)
+        result = builder.nextRunList(3)
+        assertEquals(expected.plusDays(32), result[0])
+        assertEquals(expected.plusDays(35), result[1])
+        assertEquals(expected.plusDays(37), result[2])
+        builder.seconds(0).months(1).daysOfWeek(1 at 6)
+        assertEquals("0 0 0 ? 1 6/1 2099", builder.expression)
+        result = builder.nextRunList(2)
+        assertEquals(expected.plusDays(4), result[0])
+        assertEquals(expected.plusDays(5), result[1])
+        builder.daysOfWeek(3 at 6)
+        assertEquals("0 0 0 ? 1 6/3 2099", builder.expression)
+        result = builder.nextRunList(2)
+        assertEquals(expected.plusDays(4), result[0])
+        assertEquals(expected.plusDays(11), result[1])
+        builder.daysOfWeek(2 on 5)
+        assertEquals("0 0 0 ? 1 2#5 2099", builder.expression)
+        assertEquals(expected.plusDays(28), builder.nextRun)
     }
 }
--- src/commonTest/kotlin/com/ucasoft/kcron/builders/DaysOfWeekBuilderTests.kt
+++ src/commonTest/kotlin/com/ucasoft/kcron/builders/DaysOfWeekBuilderTests.kt
@@ -1,7 +1,7 @@
 package com.ucasoft.kcron.builders
 
 import com.ucasoft.kcron.common.DayOfWeekGroups
-import kotlin.test.BeforeTest
+import com.ucasoft.kcron.common.WeekDays
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -9,17 +9,13 @@ class DaysOfWeekBuilderTests {
 
     private lateinit var builder : DaysOfWeekBuilder
 
-    @BeforeTest
-    fun setupOnce() {
-        builder = DaysOfWeekBuilder()
-    }
-
     @Test
     fun buildDaysOfWeek() {
+        builder = DaysOfWeekBuilder()
         builder.build(DayOfWeekGroups.Any, "*")
         assertEquals(listOf(1..7).flatten(), builder.daysOfWeek)
-        builder.build(DayOfWeekGroups.Specific, "MON,TUE,THU,FRI")
-        assertEquals(listOf(2, 3, 5, 6), builder.daysOfWeek)
+        builder.build(DayOfWeekGroups.Specific, "MON,TUE,THU,FRI,SUN")
+        assertEquals(listOf(1, 2, 4, 5, 7), builder.daysOfWeek)
         builder.build(DayOfWeekGroups.Specific, "1,3,5,7")
         assertEquals(listOf(1, 3, 5, 7), builder.daysOfWeek)
         builder.build(DayOfWeekGroups.EveryStartingAt, "2/2")
@@ -28,5 +24,19 @@ class DaysOfWeekBuilderTests {
         assertEquals(listOf(-2), builder.daysOfWeek)
         builder.build(DayOfWeekGroups.OfMonth, "1#5")
         assertEquals(listOf(10, 50), builder.daysOfWeek)
+    }
+
+    @Test
+    fun firstDaySunday() {
+        builder = DaysOfWeekBuilder(WeekDays.Sunday)
+        builder.build(DayOfWeekGroups.Specific, "MON,TUE,THU,FRI,SUN")
+        assertEquals(listOf(1, 2, 3, 5, 6), builder.daysOfWeek)
+    }
+
+    @Test
+    fun firstDayWednesday() {
+        builder = DaysOfWeekBuilder(WeekDays.Wednesday)
+        builder.build(DayOfWeekGroups.Specific, "MON,TUE,THU,FRI,SUN")
+        assertEquals(listOf(2, 3, 5, 6, 7), builder.daysOfWeek)
     }
 }
