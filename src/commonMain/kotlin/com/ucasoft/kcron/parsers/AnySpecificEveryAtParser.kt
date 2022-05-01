ace6af0ecd78afbf4c34025b38ea04d13093d74f
Sergey Antonov <AntonovSergeyS@gmail.com>
2022-05-01 13:33:03 +0200

    Add Starting At short week day name parsing support

--- src/commonMain/kotlin/com/ucasoft/kcron/parsers/AnySpecificEveryAtParser.kt
+++ src/commonMain/kotlin/com/ucasoft/kcron/parsers/AnySpecificEveryAtParser.kt
@@ -6,9 +6,16 @@ abstract class AnySpecificEveryAtParser<T>(
     anyPattern: String,
     specificNumberPattern: String,
     specificNamePattern: String = "",
-    secondLimitPattern: String = "")
+    private val secondLimitPattern: String = "")
         : AnySpecificParser<T>(anyPattern, specificNumberPattern, specificNamePattern)
         where T: Enum<T>, T: CronGroups {
 
-    override val additionalParts: String = "|((?:${specificNumberPattern})/(?:${secondLimitPattern.ifEmpty { specificNumberPattern }}))"
+    override val additionalParts: String
+        get() {
+            if (specificNamePattern.isNotEmpty()) {
+                return "|((?:(?:${specificNumberPattern})|(?:${specificNamePattern}))/(?:${secondLimitPattern.ifEmpty { specificNumberPattern }}))"
+            }
+
+            return "|((?:${specificNumberPattern})/(?:${secondLimitPattern.ifEmpty { specificNumberPattern }}))"
+        }
 }
--- src/commonMain/kotlin/com/ucasoft/kcron/parsers/AnySpecificParser.kt
+++ src/commonMain/kotlin/com/ucasoft/kcron/parsers/AnySpecificParser.kt
@@ -5,7 +5,7 @@ import com.ucasoft.kcron.common.CronGroups
 abstract class AnySpecificParser<T>(
     private val anyPattern: String,
     protected val specificNumberPattern: String,
-    private val specificNamePattern: String = "") : BaseParser<T>() where T: Enum<T>, T: CronGroups {
+    protected val specificNamePattern: String = "") : BaseParser<T>() where T: Enum<T>, T: CronGroups {
 
     abstract val additionalParts: String
 
