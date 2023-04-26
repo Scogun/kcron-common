7d08910f3316ac627fb659261c3f3eba798af020
Sergey <AntonovSergeyS@gmail.com>
2023-04-26 20:51:29 +0200

    Feature/updates (#12)
    * Update Gradle and Kotlin
    
    * Remove obsolete repository
    
    * Switch Settings on Kotlin
    
    * Switch Build to Koltin
    
    * Fix rebase issues
    
    * Update Gradle
    
    * Update Kotlin
    
    * Add Test report
    
    * Increase version
    
    * Revert Gradle to 7th version
    
    * Fix artifact id

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # KCron Common
 Cron realization for Kotlin Multiplatform
 
-[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=Scogun_kcron-common&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=Scogun_kcron-common) ![GitHub](https://img.shields.io/github/license/Scogun/kcron-common?color=blue) ![Publish workflow](https://github.com/Scogun/kcron-common/actions/workflows/publish.yml/badge.svg) [![Maven Central with version prefix filter](https://img.shields.io/maven-central/v/com.ucasoft.kcron/kcron-common/0.5.4?color=blue)](https://search.maven.org/artifact/com.ucasoft.kcron/kcron-common/0.5.4/jar)
+[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=Scogun_kcron-common&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=Scogun_kcron-common) ![GitHub](https://img.shields.io/github/license/Scogun/kcron-common?color=blue) ![Publish workflow](https://github.com/Scogun/kcron-common/actions/workflows/publish.yml/badge.svg) [![Maven Central with version prefix filter](https://img.shields.io/maven-central/v/com.ucasoft.kcron/kcron-common/0.6.0?color=blue)](https://search.maven.org/artifact/com.ucasoft.kcron/kcron-common/0.6.0/jar)
 
 ### Features
 * Kotlin Multiplatform library
@@ -38,7 +38,7 @@ kotlin {
     sourceSets {
         commonMain {
             dependencies {
-                implementation 'com.ucasoft.kcron:kcron-common:0.5.4'
+                implementation 'com.ucasoft.kcron:kcron-common:0.6.0'
             }
         }
     }
@@ -91,6 +91,6 @@ builder.years(2021..2025)
 println(builder.expression) // 0/10 5-25 5,12 ? * SUN#5 2021-2025
 ``` 
 ### Current status
-This library is on beta version `0.5.4`.
+This library is on beta version `0.6.0`.
 However, it will be a part of another cool library.
 Check the news! 
---
+++ build.gradle.kts
@@ -0,0 +1,118 @@
+plugins {
+    kotlin("multiplatform") version "1.8.20"
+    id("maven-publish")
+    id("signing")
+}
+
+group = "com.ucasoft.kcron"
+version = "0.6.0"
+
+repositories {
+    mavenCentral()
+}
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = JavaVersion.VERSION_1_8.toString()
+        }
+    }
+    linuxX64()
+    mingwX64()
+    macosX64()
+    js(IR) {
+        browser()
+        nodejs()
+    }
+    ios()
+    iosSimulatorArm64()
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
+            }
+        }
+        commonTest {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+        val jsTest by getting {
+            dependencies {
+                implementation(kotlin("test-js"))
+            }
+        }
+        val jvmTest by getting {
+            dependencies {
+                implementation(kotlin("test-junit"))
+            }
+        }
+    }
+}
+
+val stubSources by tasks.creating(Jar::class) {
+    archiveClassifier.set("sources")
+}
+
+val stubJavadoc by tasks.creating(Jar::class) {
+    archiveClassifier.set("javadoc")
+}
+
+val sourceJar by tasks.creating(Jar::class) {
+    archiveClassifier.set("sources")
+    from(kotlin.sourceSets.commonMain.get().kotlin)
+}
+
+publishing {
+    publications.configureEach {
+        if (this is MavenPublication) {
+            val libraryType = name
+            artifactId = when (libraryType) {
+                "js", "metadata" -> "${project.name}-${libraryType}"
+                "jvm" -> project.name
+                "kotlinMultiplatform" -> "${project.name}-native"
+                else -> artifactId
+            }
+            if (libraryType != "kotlinMultiplatform") {
+                artifact(stubJavadoc)
+            }
+            pom {
+                name.set("KCron Common")
+                description.set("Cron realization for Kotlin Multiplatform")
+                url.set("https://github.com/Scogun/kcron-common")
+                licenses {
+                    license {
+                        name.set("GPL-3.0 License")
+                        url.set("https://www.gnu.org/licenses/gpl-3.0.en.html")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("Scogun")
+                        name.set("Sergey Antonov")
+                        email.set("SAntonov@ucasoft.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/Scogun/kcron-common.git")
+                    developerConnection.set("scm:git:ssh://github.com:Scogun/kcron-common.git")
+                    url.set("https://github.com/Scogun/kcron-common")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "MavenCentral"
+            url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_PASSWORD")
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications)
+}
