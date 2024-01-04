086a775590bed68a43f7b63791a6f366399041a7
Alexei Myshkouski <alexeimyshkouski@gmail.com>
2024-01-04 09:54:32 +0300

    KCron -> Cron

--- src/commonMain/kotlin/com/ucasoft/kcron/Cron.kt
+++ src/commonMain/kotlin/com/ucasoft/kcron/Cron.kt
@@ -5,7 +5,7 @@ import com.ucasoft.kcron.common.WeekDays
 import com.ucasoft.kcron.parsers.Parser
 import com.ucasoft.kcron.settings.Settings
 
-class KCron {
+class Cron {
 
     companion object {
 
@@ -21,3 +21,9 @@ class KCron {
         }
     }
 }
+
+@Deprecated(
+    message = "Class name should not explicitly tell it's written in Kotlin (with starting 'K'). It would be compiled in Java bytecode, Javascript, etc.",
+    replaceWith = ReplaceWith("Cron")
+)
+typealias KCron = Cron
