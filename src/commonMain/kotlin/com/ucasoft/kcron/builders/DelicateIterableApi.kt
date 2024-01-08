bf5b25d871ba88fdb6bee31f27c27a7b25d5d4eb
Alexei <alexeimyshkouski@gmail.com>
2024-01-08 15:50:54 +0300

    fix spelling

--- src/commonMain/kotlin/com/ucasoft/kcron/builders/DelicateIterableApi.kt
+++ src/commonMain/kotlin/com/ucasoft/kcron/builders/DelicateIterableApi.kt
@@ -1,7 +1,7 @@
 package com.ucasoft.kcron.builders
 
 @RequiresOptIn(
-    message = "This method returns and Iterable that possibly infinite. Be sure to limit total size when collecting result.",
+    message = "This method returns Iterable that possibly infinite. Be sure to limit elements count when collecting result.",
 )
 @Target(AnnotationTarget.FUNCTION)
 annotation class DelicateIterableApi
