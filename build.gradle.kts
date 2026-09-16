0429b7ec3d021d5cd61bc55b0604a47a984e879b
Sergey <AntonovSergeyS@gmail.com>
2026-09-16 08:14:47 +0200

    Fix Wasm (#44)

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ allprojects {
         google()
         mavenCentral()
     }
-    version = "0.33.8"
+    version = "0.33.9"
 
     tasks.withType<Test> {
         reports {
--- kcron-ui-builder/build.gradle.kts
+++ kcron-ui-builder/build.gradle.kts
@@ -19,6 +19,7 @@ kotlin {
     wasmJs {
         browser()
         nodejs()
+        binaries.executable()
     }
     iosArm64()
     iosSimulatorArm64()
